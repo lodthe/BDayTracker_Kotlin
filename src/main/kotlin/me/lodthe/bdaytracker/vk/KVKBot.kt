81081c27d456e4e9ed32a1653910e0b9e7d28b17
Igor Baliuk <minefoggiar@gmail.com>
2020-01-11 14:25:11 +0300

    Now VK requests will execute in coroutine

--- src/main/kotlin/me/lodthe/bdaytracker/vk/KVKBot.kt
+++ src/main/kotlin/me/lodthe/bdaytracker/vk/KVKBot.kt
@@ -2,18 +2,18 @@ package me.lodthe.bdaytracker.vk
 
 import com.vk.api.sdk.client.Lang
 import com.vk.api.sdk.client.VkApiClient
-import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.client.actors.ServiceActor
+import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.objects.friends.responses.GetFieldsResponse
-import com.vk.api.sdk.objects.friends.responses.GetResponse
 import com.vk.api.sdk.objects.users.Fields
-import com.vk.api.sdk.objects.users.UserXtrCounters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class KVKBot(private val APP_ID: Int, private val APP_TOKEN: String) {
     private val vk = VkApiClient(HttpTransportClient())
     private val actor = ServiceActor(APP_ID, APP_TOKEN)
 
-    fun getFriendList(userId: Int): GetFieldsResponse? {
-        return vk.friends().getWithFields(actor, Fields.BDATE).userId(userId).lang(Lang.RU).execute()
+    suspend fun getFriendList(userId: Int): GetFieldsResponse? = withContext(Dispatchers.IO) {
+        vk.friends().getWithFields(actor, Fields.BDATE).userId(userId).lang(Lang.RU).execute()
     }
 }
